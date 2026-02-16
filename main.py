591d48b851722a77861cfff1dc71310b0de2b831
Sampath Kumar <sampath.k.kumar@gmail.com>
2026-02-16 01:58:58 -0500

    initial clean setup without secrets

---
+++ main.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+
+# import os
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!\n")
+    # print("============================\n")
+    # print("printing environment variables...openai_api_key\n")
+    # print(os.environ.get("OPENAI_API_KEY")+"\n")
+    # print("printing environment variables...google_api_key\n")
+    # print(os.environ.get("GOOGLE_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
