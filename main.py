45e4083567a08dd7ecc3a81318c332ba8c2bcfd9
Sampath Kumar <sampath.k.kumar@gmail.com>
2026-02-22 14:47:28 -0500

    updating the main changes

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain import OpenAI
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+from langchain.openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+
+
 def main():
     print("Hello from langchain-course!")
 
